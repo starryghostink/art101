76ba945dbfb1d97eaac38559cd1ba8271dffadfa
starryghostink <chplam@ucsc.edu>
2025-04-23 18:54:05 -0700

    lab6

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,6 +1,6 @@
 // index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Chris Lam
+// Date: 4/23/25
 
 // Constants
 
@@ -14,7 +14,23 @@ function myFunction(param1, param2) {
 
 function main() {
   console.log("Main function started.");
-  // the code that makes everything happen
+  // Define Variables 
+  let myTransport = ["bus", "legs", "uber"];
+
+  //create an object for my main ride
+  myMainRide = {
+    make: "Audi",
+    model: "Q5",
+    color: "forest green",
+    year: 2021,
+    age: function() {
+        return 2025 - this.year;
+    }
+  };
+  //output
+  document.writeln("Kinds of transportation I use: ", myTransport, "</br>");
+  document.writeln("My Main Ride: <pre>",
+      JSON.stringify(myMainRide, null, '\t'), "</pre>");
 }
 
 // let's get this party started
