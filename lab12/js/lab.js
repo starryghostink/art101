251e3254dcd312b2b0f4173e909cae17b0430df8
starryghostink <chplam@ucsc.edu>
2025-05-07 17:27:23 -0700

    lab10

---
+++ lab12/js/lab.js
@@ -0,0 +1,14 @@
+// index.js - purpose and description here
+// Author: Chris Lam
+// Date: 4/30/25
+
+// Constants
+
+// Functions
+
+// this is an example function and this comment tells what it doees and what parameters are passed to it.
+function myFunction(param1, param2) {
+  // some code here
+  // return results;
+}
+
