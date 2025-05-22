850b971ddacc37014e10727e6ea44c56c2f2839f
starryghostink <chplam@ucsc.edu>
2025-05-21 17:46:03 -0700

    lab12

--- lab12/js/lab.js
+++ lab12/js/lab.js
@@ -1,6 +1,6 @@
-// index.js - purpose and description here
+// index.js - sort someone into a house based on their name
 // Author: Chris Lam
-// Date: 4/30/25
+// Date: 5/21/25
 
 // Constants
 
@@ -12,3 +12,39 @@ function myFunction(param1, param2) {
   // return results;
 }
 
+function choosyHat(name) {
+  let length=name.length
+  let str = "";
+  mod = length % 9;
+
+  if (name[0].toLowerCase() == 'b' && mod == 5) {
+    str = "Emperor Coven"
+  } else if (mod == 0) {
+    str = "Construction Coven";
+  } else if (mod == 1) {
+    str = "Potions Coven";
+  } else if (mod == 2) {
+    str = "Healing Coven";
+  } else if (mod == 3) {
+    str = "Bard Coven";
+  } else if (mod == 4) {
+    str = "Oracle Coven"; 
+  } else if (mod == 5) {
+    str = "Abomination Coven";
+  } else if (mod == 6) {
+    str = "Beast Keeping Coven";
+  } else if (mod == 7) {
+    str = "Illusion Coven";
+  } else if (mod == 8) {
+    str = "Plant Coven";
+  }
+  return str;
+}
+
+$("#button").click(function(){
+  let name =$("#input").val();
+  console.log(name);
+  let coven = choosyHat(name);
+  console.log(coven);
+  $("#output").html("<h1>" + "The Choosy Hat has put you in " + coven + "</h1>");
+})
