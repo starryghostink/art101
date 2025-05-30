bb43c4c4a51532a5f3838a5a6d203d23591b9582
starryghostink <chplam@ucsc.edu>
2025-05-29 18:29:08 -0700

    lab15

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -1,6 +1,6 @@
 // index.js - purpose and description here
 // Author: Chris Lam
-// Date: 4/30/25
+// Date: 5/29/25
 
 // Constants
 
@@ -11,4 +11,27 @@ function myFunction(param1, param2) {
   // some code here
   // return results;
 }
+// constant/our url
+const ajaxPokemon = {
+    url: "https://pokeapi.co/api/v2/pokemon?limit=100/",
+    type: "GET",
+    dataType: "JSON"
+}
+// event listener
+$("#activate").click(function(){
+console.log("Here's a Pokemon");
+// generate random pokemon
+$.ajax(ajaxPokemon)
+  .done(function(data){
+   console.log("Boom");
+   console.log(data);
+   let results = data.results;
+   let randomIndex = Math.floor(Math.random() * results.length);
+   let randomPokemon = results[randomIndex].name;
+   $("#output").html("<h1>" + randomPokemon);
+   })
+   .fail(function(xhr, status, errorThrown) {
+   console.log(errorThrown + " Status:" + status);
+  })
+})
 
