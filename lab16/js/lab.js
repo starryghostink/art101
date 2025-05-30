113b9c2033358d6c4a1c1709a1ad51b83a89cdb2
starryghostink <chplam@ucsc.edu>
2025-05-29 19:13:45 -0700

    lab16

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -12,3 +12,24 @@ function myFunction(param1, param2) {
   // return results;
 }
 
+// constant/our url
+const comicObj = {
+    url: "https://xkcd.com/514/info.0.json",
+    type: "GET",
+    dataType: "JSON"
+}
+
+// generate random comic
+$.ajax(comicObj)
+  .done(function(data){
+   console.log("Boom");
+   console.log(data);
+   let results = data.results;
+   let randomIndex = Math.floor(Math.random() * results.length);
+   let randomComic = results[randomIndex].name;
+   $("#output").html("<h1>" + randomComic);
+   })
+   .fail(function(xhr, status, errorThrown) {
+   console.log(errorThrown + " Status:" + status);
+  })
+
